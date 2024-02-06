3550ed75a235dac8d107f51018631791839bc24e
Lazizbek6766 <turayevlazizbek6766@gmail.com>
2024-02-06 18:10:25 +0500

    commit

--- app/src/main/java/com/example/myapplicationpay/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationpay/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.ThumbUp
@@ -39,90 +41,50 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    var isExpanded by remember {
-                        mutableStateOf(false)
-                    }
 
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        verticalArrangement = Arrangement.SpaceAround,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
+                    val list = listOf("Salom","Salom","Salom","Salom","Salom","Salom","Salom","Salom","Salom","Salom","Salom","Salom","Salom","Salom","Salom","Salom")
+                    LazyColumn(
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        content = {
+                        items(list) { item ->
 
-                        SwipeActionsRight(
-                            modifier = Modifier
-                                .fillMaxWidth(0.95f)
-                                .height(100.dp),
-                            numberOfActions = 1,
-                            isExpanded = isExpanded,
-                            onChangedCard = { isExpanded = it },
-                            actionOneImage = Icons.Default.Share,
-                            actionOneColor = Color(0xFF005EFF)
-                        ) {
-                            Row(
+                            var isExpanded by remember {
+                                mutableStateOf(false)
+                            }
+
+                            SwipeActionsRight(
                                 modifier = Modifier
-                                    .fillMaxWidth()
-                                    .fillMaxHeight()
-                                    .padding(15.dp),
-                                horizontalArrangement = Arrangement.Center
+                                    .fillMaxWidth(0.95f)
+                                    .height(100.dp),
+                                numberOfActions = 1,
+                                isExpanded = isExpanded,
+                                onChangedCard = { isExpanded = it },
+                                actionOneImage = Icons.Default.Share,
+                                actionOneColor = Color(0xFF005EFF)
                             ) {
-                                Column(
-                                    modifier = Modifier.fillMaxHeight(),
-                                    verticalArrangement = Arrangement.SpaceAround
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .fillMaxHeight()
+                                        .padding(15.dp),
+                                    horizontalArrangement = Arrangement.Center
                                 ) {
-                                    Text(
-                                        text = "Swipe to share",
-                                        color = Color.Black,
-                                        fontSize = 18.sp,
-                                    )
+                                    Column(
+                                        modifier = Modifier.fillMaxHeight(),
+                                        verticalArrangement = Arrangement.SpaceAround
+                                    ) {
+                                        Text(
+                                            text = item,
+                                            color = Color.Black,
+                                            fontSize = 18.sp,
+                                        )
+
+                                    }
 
                                 }
-
                             }
                         }
-
-                        SwipeActionsRight(
-                            modifier = Modifier
-                                .fillMaxWidth(0.95f)
-                                .height(100.dp),
-                            itemWidth = 200.dp,
-                            isExpanded = isExpanded,
-                            actionOneImage = Icons.Default.Share,
-                            actionTwoImage = Icons.Default.ThumbUp,
-                            onChangedCard = { isExpanded = it }) {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .fillMaxHeight()
-                                    .padding(15.dp),
-                                horizontalArrangement = Arrangement.SpaceAround
-                            ) {
-                                Column(
-                                    modifier = Modifier.fillMaxHeight(),
-                                    verticalArrangement = Arrangement.SpaceAround
-                                ) {
-                                    Text(
-                                        text = "Swipe Actions",
-                                        color = Color.Black,
-                                        fontSize = 18.sp
-                                    )
-
-                                    Text(
-                                        text = "For more options, swipe the card",
-                                        color = Color.Black,
-                                        fontSize = 16.sp
-                                    )
-                                }
-
-                                Icon(
-                                    imageVector = Icons.Default.ThumbUp, contentDescription = null,
-                                    modifier = Modifier.fillMaxHeight(), tint = Color.Gray
-                                )
-
-                            }
-                        }
-                    }
+                    })
                 }
             }
         }
